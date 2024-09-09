579f9c9bf3504aaf0adcc08f8d859897db355e23
Raizinhoo <gersinhowasd@gmail.com>
2024-09-09 11:38:44 -0300

    Criação API Autor

--- app/urls.py
+++ app/urls.py
@@ -12,9 +12,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
 from core.views import EditoraViewSet
+from core.views import AutorViewSet
 
 router = DefaultRouter()
 
+router.register(r"autores", AutorViewSet,)
 router.register(r"categorias", CategoriaViewSet,)
 router.register(r"editoras", EditoraViewSet,)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
