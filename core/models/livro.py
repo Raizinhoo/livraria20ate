46aef108de909990d12a6a1a570224823873e740
Raizinhoo <gersinhowasd@gmail.com>
2024-09-10 10:13:46 -0300

    Dois serializadores para livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from .categoria import Categoria
 from .editora import Editora
+from .autor import Autor
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -9,6 +10,7 @@ class Livro(models.Model):
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     
+    autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
     
@@ -19,5 +21,7 @@ class Livro(models.Model):
 
         verbose_name = "Livro"
         verbose_name_plural = "Livros"
+        
+    
     
     
