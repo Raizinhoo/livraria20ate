ffd446c4945c19ebaeb664f4854fd9ccd6405ba6
Raizinhoo <gersinhowasd@gmail.com>
2024-10-08 10:54:41 -0300

    27 - Inclusão do total da compra na listagem de compras

--- core/models/compra.py
+++ core/models/compra.py
@@ -12,9 +12,12 @@ class StatusCompra(models.IntegerChoices):
 class Compra(models.Model):        
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)        
+    @property
+    def total(self):
+            return sum(item.livro.preco * item.quantidade for item in self.itens.all())
     
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra ,on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
-    
+
