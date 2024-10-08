61586d92a18515dab5a3b3211c755825649a77ba
Raizinhoo <gersinhowasd@gmail.com>
2024-10-08 10:44:01 -0300

    Limitando informações do livro na api

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,13 +1,26 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from core.models import Compra, ItensCompra
+from core.models import Compra, ItensCompra, Livro
 
+class LivroSimplificadoSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ("quantidade", "titulo", "capa", "editora", "categoria", "autor") 
 
 class ItensCompraSerializer(ModelSerializer):
+    
+    livro = LivroSimplificadoSerializer(read_only=True)
+    
+    total = SerializerMethodField()
+    
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
+    
     class Meta:
         model = ItensCompra
-        fields = ("livro", "quantidade") 
+        fields = ("livro", "quantidade", "total") 
         depth = 1
+        
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
