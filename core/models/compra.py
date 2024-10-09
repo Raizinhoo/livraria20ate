9b78d00bd72ac6f3e275d521e0d06adaac834a1d
Raizinhoo <gersinhowasd@gmail.com>
2024-10-09 16:51:08 -0300

    27 - incompleta

--- core/models/compra.py
+++ core/models/compra.py
@@ -20,4 +20,8 @@ class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra ,on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
+    
+#     def __str__(self):
+#         return f"({self.id}) {self.livro}"
+       
 
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -21,6 +21,11 @@ class ItensCompraSerializer(ModelSerializer):
         fields = ("livro", "quantidade", "total") 
         depth = 1
         
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+        
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
@@ -28,5 +33,20 @@ class CompraSerializer(ModelSerializer):
     itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
-        fields = ("id", "usuario", "status", "total", "itens") 
+        fields = ("id", "usuario", "status", "total", "itens")
+        
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
+    
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+        
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
         
