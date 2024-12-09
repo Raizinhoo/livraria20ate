24b6bbebe662f35e8a5f3903ca9a843c9726593c
Raizinhoo <gersinhowasd@gmail.com>
2024-12-09 11:40:31 -0300

    Gravando o preço do livro no item da compra --32

--- core/models/compra.py
+++ core/models/compra.py
@@ -14,12 +14,13 @@ class Compra(models.Model):
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)        
     @property
     def total(self):
-            return sum(item.livro.preco * item.quantidade for item in self.itens.all())
+            return sum(item.preco * item.quantidade for item in self.itens.all())
     
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra ,on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     
     def __str__(self):
         return f"({self.id}) {self.livro.titulo}({self.livro.quantidade}) R${self.livro.preco}"
--- core/serializers/editora.py
+++ core/serializers/editora.py
@@ -3,10 +3,11 @@ from rest_framework.serializers import ModelSerializer
 from core.models import Editora
 
 class EditoraSerializer(ModelSerializer):
+
+    class Meta:
+        model = Editora
+        fields = "__all__"
     
     def validate_email(self, email):
         return email.lower()
     
-    class Meta:
-        model = Editora
-        fields = "__all__"
