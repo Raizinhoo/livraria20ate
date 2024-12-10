765348fb3a13c2185bb800c22e6123c4f93e3b69
Raizinhoo <gersinhowasd@gmail.com>
2024-12-10 11:24:08 -0300

    Ajustando estoque de livros

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -4,6 +4,7 @@ from rest_framework.serializers import (
     Serializer,
     SlugRelatedField,
     ValidationError,
+    IntegerField,
 )
 
 from core.models import Livro
@@ -11,30 +12,48 @@ from core.models import Livro
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
-class LivroSerializer(ModelSerializer):
-    capa_attachment_key = SlugRelatedField(source="capa", queryset=Image.objects.all(), slug_field="attachment_key", required=False, write_only=True)
-    capa = ImageSerializer(required=False, read_only=True)
-    class Meta:
-         model = Livro
-         fields = "__all__"
 
-        
+class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="capa", queryset=Image.objects.all(), slug_field="attachment_key", required=False, write_only=True
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+
 class LivroDetailSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
         depth = 1
         capa = ImageSerializer(required=False)
-        
+
+
 class LivroListSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = ("id", "titulo", "preco")
-        
+
+
 class LivroAlterarPrecoSerializer(Serializer):
     preco = DecimalField(max_digits=10, decimal_places=2)
-    
+
     def validate_preco(self, value):
         if value <= 0:
             raise ValidationError("O Preço Precisa ser Positivo, Cabeção!")
+        return value
+
+
+class LivroAjustarEstoqueSerializer(Serializer):
+    quantidade = IntegerField()
+
+    def validate_quantidade(self, value):
+        livro = self.context.get("livro")
+        if livro:
+            nova_quantidade = livro.quantidade + value
+            if nova_quantidade < 0:
+                raise ValidationError("Quantidade em estoque não pode ser negativa")
         return value
--- core/views/livro.py
+++ core/views/livro.py
@@ -6,34 +6,53 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
 from core.serializers import (
-    LivroDetailSerializer, 
-    LivroListSerializer, 
+    LivroDetailSerializer,
+    LivroListSerializer,
     LivroSerializer,
-    LivroAlterarPrecoSerializer)
+    LivroAlterarPrecoSerializer,
+    LivroAjustarEstoqueSerializer,
+)
 
 
-    
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    
+
     def get_serializer_class(self):
         if self.action == "list":
             return LivroListSerializer
         elif self.action == "retrieve":
             return LivroDetailSerializer
         return LivroSerializer
-    
+
     @action(detail=True, methods=["patch"])
     def alterar_preco(self, request, pk=None):
         livro = self.get_object()
-        
+
         serializer = LivroAlterarPrecoSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
+
         livro.preco = serializer.validated_data["preco"]
         livro.save()
-        
+
         return Response(
             {"detail": f"Preco do Livro '{livro.titulo}' atualizado para {livro.preco}."}, status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=["post"])
+    def alterar_quantidade(self, request, pk=None):
+        livro = self.get_object()
+
+        serializer = LivroAjustarEstoqueSerializer(data=request.data, context={"livro": livro})
+        serializer.is_valid(raise_exception=True)
+
+        # breakpoint()
+
+        quantidade_ajuste = serializer.validated_data["quantidade"]
+
+        livro.quantidade += quantidade_ajuste
+        livro.save()
+
+        return Response(
+            {"status": "Quantidade mudada com sucesso", "novo_estoque": livro.quantidade}, status=status.HTTP_200_OK
         )
